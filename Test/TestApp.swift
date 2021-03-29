c68bb0c2a2f35eee9d24c73fa20c848720d91be5
Alžbeta Gogoláková <alzbeta.gogolakova@quanti.cz>
2021-03-29 22:49:20 +0200

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Alžbeta Gogoláková on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
